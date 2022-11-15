2d80bcd4074e6acda570e66245f07a6c16baa8a4
Fabio <fabiochirinos7@gmail.com>
2022-11-15 12:56:49 -0500

    se agrego la propiedad texto al componente Tarea

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Encabezado from './components/Encabezado';
+import Tarea from './components/Tarea';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Encabezado>Aplicación de tareas</Encabezado>
       <div className='tareas-lista-principal'>
         <h2>Mis tareas</h2>
+        <Tarea texto='Aprender React' />
       </div>
     </div>
   );
