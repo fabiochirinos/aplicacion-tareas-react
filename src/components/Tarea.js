f2613b0f8b5e52ae6240624badb7733b732c9d98
Fabio <fabiochirinos7@gmail.com>
2022-11-15 16:29:22 -0500

    se agrego otro props (completada) para el componente Tarea y se modifico su estilo de la nueva clase

--- src/components/Tarea.js
+++ src/components/Tarea.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/Tarea.css'
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 export default function Tarea({ texto }) {
   return (
@@ -7,8 +8,8 @@ export default function Tarea({ texto }) {
       <div className='tarea-texto'>
         {texto}
       </div>
-      <div className='tarea-icono'>
-        Eliminar
+      <div className='tarea-contenedor-iconos'>
+      <AiOutlineCloseCircle className='tarea-icono' />
       </div>
     </div>
   )
